ce71688898935aa615226b5f73de1df08fed8717
Sumit Patel <sumitpatel813@gmail.com>
2025-07-07 00:12:48 +0530

    Multiple Senders. Refactor. Fixed hanging test case.

--- crust_of_rust/src/channels.rs
+++ crust_of_rust/src/channels.rs
@@ -15,7 +15,7 @@ use std::{
 };
 
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 // #[derive(Clone)] would be wrong cause it would have the following signature:
@@ -23,10 +23,30 @@ pub struct Sender<T> {
 // This is incorrect for our context cause Arc makes the interior value clonable irrespective of it being clone or not, and we want to keep it that way.
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        // Same reason as others to avoid deadlocking / infinitely holding the lock.
+        drop(inner);
+
         Sender {
-            // Here we explicitly call Arc::clone cause self.inner.clone would be ambigious. Reason being, Arc dereferences to T (the enclosed type) and if that type T is also clone,
+            // Here we explicitly call Arc::clone cause self.shared.clone would be ambigious. Reason being, Arc dereferences to T (the enclosed type) and if that type T is also clone,
             // we're going dark places.
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let is_last_sender = inner.senders == 1;
+        // Same reason as others to avoid deadlocking / infinitely holding the lock.
+        drop(inner);
+
+        // Notify the receiver in case it was wistfully waiting while the last of the senders bite the dust.
+        if is_last_sender {
+            self.shared.signal_data_sent.notify_one();
         }
     }
 }
@@ -35,60 +55,71 @@ impl<T> Sender<T> {
     pub fn send(&mut self, data_to_send: T) {
         // You could wrap the next two lines in a block and the queue would be dropped implicitly but, we'd rather be explicit. Makes it easier to read.
         // These things do return Poision struct which tells us if the other thread has panicked we've ignored it for now.
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(data_to_send);
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(data_to_send);
         // We need to drop the queue and its held lock otherwise the other thread would wake up but never get the Mutex, likely a deadlock.
-        drop(queue);
-        self.inner.signal_data_sent.notify_one();
+        drop(inner);
+
+        self.shared.signal_data_sent.notify_one();
     }
 }
 
 // The Reciever needs to have a mutex despite there being only one consumer is because a send and receive could happen at the same time, which would likely lead to problems.
 // To avoid those we have Mutex on both sender and receiver.
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
-    pub fn receive(&mut self) -> T {
+    pub fn receive(&mut self) -> Option<T> {
         // These things do return Poision struct which tells us if the other thread has panicked we've ignored it for now.
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut inner = self.shared.inner.lock().unwrap();
 
         // We loop but it's not a spinlock type. The condvar makes sure that the thread sleeps when we hit None arm.
         // When the receiver signals the thread is woken up again.
         loop {
-            match queue.pop_front() {
+            match inner.queue.pop_front() {
                 // We retun from here so the Mutex is dropped anyways, no need for explicit mention here.
-                Some(data) => return data,
+                Some(data) => return Some(data),
+                None if inner.senders == 0 => return None,
                 None => {
                     // wait automatically dros the Mutex so the thread that needs to wake this up can acquire the shred resource. Otherwise you guessed it, it's a deadlock.
-                    queue = self.inner.signal_data_sent.wait(queue).unwrap();
+                    inner = self.shared.signal_data_sent.wait(inner).unwrap();
                 }
             }
         }
     }
 }
 
-// The Mutex is on inner
+// Holds the data structure through which data is exchanged and the number of currently active senders.
 struct Inner<T> {
-    queue: Mutex<VecDeque<T>>,
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
+// Holds inner struct for state, and a conditional variable to signal that one of the senders has sent data that the receiver can consume.
+struct Shared<T> {
+    inner: Mutex<Inner<T>>,
     signal_data_sent: Condvar,
 }
 
 // Its convention to return the Sender first and then the Receiver.
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Inner {
-        queue: Mutex::default(),
+    let shared = Shared {
+        inner: Mutex::new(Inner {
+            queue: VecDeque::new(),
+            senders: 1,
+        }),
         signal_data_sent: Condvar::new(),
     };
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
 
     (
         Sender {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     )
 }
@@ -101,15 +132,16 @@ mod tests {
     fn ping_pong() {
         let (mut sender, mut receiver) = channel();
         sender.send(42);
-        assert_eq!(receiver.receive(), 42);
+        assert_eq!(receiver.receive(), Some(42));
     }
 
-    // For poorly designed funciton this one hangs.
+    // For poorly designed funciton this one hangs
     #[test]
     fn drop_sender_instantly() {
         // Cause we're not really sending anything and receiving, we need to specify a type explicitly to the channel function.
         let (mut sender, mut receiver) = channel::<()>();
-        let _ = sender;
-        let _ = receiver.receive();
+        drop(sender);
+
+        assert_eq!(receiver.receive(), None);
     }
 }
